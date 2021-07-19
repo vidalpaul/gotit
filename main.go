f060c7597c6ac75a7a7acab269abd6f0f3c167b5
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-19 11:39:14 -0300

    Update main.go

--- main.go
+++ main.go
@@ -13,19 +13,28 @@ func main() {
 		"http://stackoverflow.com",
 		"http://npmjs.com",
 		"http://docs.python.org",
+		"https://rust-lang.org/",
+		"https://julialang.org/",
+		"https://dart.dev/",
 	}
+
+	c := make(chan string)
 
 	for _, l := range links {
-		checkLink(l)
+		go checkLink(l, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(l string) {
+func checkLink(l string, c chan string) {
 	_, e := http.Get(l)
 	if e != nil {
 		fmt.Println(l, "might be down.")
+		c <- "Might be down"
 		return
 	}
 
 	fmt.Println(l, "is up.")
+	c <- "Is up"
 }
